57033b730d2745c9d0a9bd4c3b6f33fcebe9ba5a
tsundesu <j2341@matsue-ct.ac.jp>
2026-07-07 07:59:59 +0000

    サインイン状態によってトップページの内容を切り替えよう

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,27 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  console.log(`isAuth: ${isAuth}`);
-
   knex("tasks")
     .select("*")
     .then(function (results) {
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const todo = req.body.add;
   knex("tasks")
     .insert({user_id: 1, content: todo})
@@ -34,6 +36,7 @@ router.post('/', function (req, res, next) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
