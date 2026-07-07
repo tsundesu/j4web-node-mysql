9f9a360b3a4b8dbe34930df846587148eb3aa11b
tsundesu <j2341@matsue-ct.ac.jp>
2026-07-07 07:45:55 +0000

    ログアウト機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -40,5 +40,6 @@ router.post('/', function (req, res, next) {
 
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
