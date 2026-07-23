26adca14caf51695793489788b198dbd3fe422ef
tsundesu <j2341@matsue-ct.ac.jp>
2026-07-23 07:09:53 +0000

    req.isAuthenticated()でサインイン状態を判断しよう

--- routes/signup.js
+++ routes/signup.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../db/knex');
+const knex = require("../db/knex");
 const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
+  const isAuth = req.isAuthenticated();
   res.render('signup', {
     title: 'Sign up',
-    isAuth: isAuth
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
+  const isAuth = req.isAuthenticated();
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
@@ -31,7 +29,6 @@ router.post('/', function (req, res, next) {
         })
       } else if (password === repassword) {
         const hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
         knex("users")
           .insert({name: username, password: hashedPassword})
           .then(function () {
